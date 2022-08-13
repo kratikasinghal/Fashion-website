248d5515c11fd3e4ca719f1733d2b5b5159f64a8
Kratika Singhal <72219837+kratikasinghal@users.noreply.github.com>
2022-08-13 18:28:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+/*
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+*/
+function first(){
+  document.getElementById('slideimage').src="images/slider img 2.png";
+}
+function second(){
+  document.getElementById('slideimage').src="images/slider img 3.png";
+}
+function third(){
+  document.getElementById('slideimage').src="images/slider img4.png";
+}
+function fourth(){
+  document.getElementById('slideimage').src="images/slider img5.png";
+}
+function fifth(){
+  document.getElementById('slideimage').src="images/slider img 6.png";
+}
+function sixth(){
+  document.getElementById('slideimage').src="images/slider img 7.png";
+}
+function seventh(){
+  document.getElementById('slideimage').src="images/slider img 8.png";
+}
+function eigth(){
+  document.getElementById('slideimage').src="images/slider image 9.png";
+}
+function ninth(){
+  document.getElementById('slideimage').src="images/slider img 10.png";
+}
+function tenth(){
+  document.getElementById('slideimage').src="images/slider img 11.png";
+}
+function eleventh(){
+  document.getElementById('slideimage').src="images/slider img 12.png";
+}
+function twelvth(){
+  document.getElementById('slideimage').src="images/slider img 13.png";
+}
+function thirteenth(){
+  document.getElementById('slideimage').src="images/slider img 14.png";
+}
+function fourthteenth(){
+  document.getElementById('slideimage').src="images/slider img 1.png";
+}
+setInterval(first,4000);
+setInterval(second,8000);
+setInterval(third,12000);
+setInterval(fourth,16000);
+setInterval(fifth,20000);
+setInterval(sixth,24000);
+setInterval(seventh,28000);
+setInterval(eigth,32000);
+setInterval(ninth,36000);
+setInterval(tenth,40000);
+setInterval(eleventh,44000);
+setInterval(twelvth,48000);
+setInterval(thirteenth,52000);
+setInterval(fourthteenth,56000);
+
+
+function one(){
+  document.getElementById("slideimage").src="images/slider img 1.png";
+}
+function two(){
+  document.getElementById("slideimage").src="images/slider img 2.png";
+}
+function three(){
+  document.getElementById("slideimage").src="images/slider img 3.png";
+}
+function four(){
+  document.getElementById("slideimage").src="images/slider img4.png";
+}
+function five(){
+  document.getElementById("slideimage").src="images/slider img5.png";
+}
+function six(){
+  document.getElementById("slideimage").src="images/slider img 6.png";
+}
+function seven(){
+  document.getElementById("slideimage").src="images/slider img 7.png";
+}
+function eight(){
+  document.getElementById("slideimage").src="images/slider img 8.png";
+}
+function nine(){
+  document.getElementById("slideimage").src="images/slider image 9.png";
+}
+function ten(){
+  document.getElementById("slideimage").src="images/slider img 10.png";
+}
+function eleven(){
+  document.getElementById("slideimage").src="images/slider img 11.png";
+}
+function twelve(){
+  document.getElementById("slideimage").src="images/slider img 12.png";
+}
+function thirteen(){
+  document.getElementById("slideimage").src="images/slider img 13.png";
+}
+function fourteen(){
+  document.getElementById("slideimage").src="images/slider img 14.png";
+}
